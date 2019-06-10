f1c2998f8039538696eb87d6966d0ec70141c8f6
shekodn <sdiazn19@gmail.com>
2019-06-10 18:50:05 -0500

    Test hook

--- utils/utils.go
+++ utils/utils.go
@@ -10,6 +10,6 @@ func Message(status bool, message string)(map[string]interface{}) {
 }
 
 func Respond(w http.ResponseWriter, data map[string] interface{}) {
-       w.Header().Add("Content-Type", "application/json")
+        w.Header().Add("Content-Type", "application/json")
     json.NewEncoder(w).Encode(data)
 }
