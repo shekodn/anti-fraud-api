b2a5182e143d04a7c48a2897e8870c5cb16526d3
shekodn <sdiazn19@gmail.com>
2019-06-11 16:24:03 -0500

    Add golint to city.go

--- models/city.go
+++ models/city.go
@@ -1,62 +1,46 @@
 package models
 
-import(
-  // "fmt"
-  "os"
-  "math"
-  s "strings"
-  "time"
+import (
+	"math"
+	"os"
+	s "strings"
+	"time"
 
-  "github.com/gocarina/gocsv"
+	"github.com/gocarina/gocsv"
 )
 
+// City representation
 type City struct {
-  Name string `csv:"city_ascii"`
-  Coordinate
-  CountryCode string `csv:"iso2"`
-  NotUsed string `csv:"-"`
+	Name string `csv:"city_ascii"`
+	Coordinate
+	CountryCode string `csv:"iso2"`
+	NotUsed     string `csv:"-"`
 }
 
+// Coordinate representation
 type Coordinate struct {
 	Lat float64 `csv:"lat"`
-  Lng float64 `csv:"lng"`
+	Lng float64 `csv:"lng"`
 }
 
 var citiesDB = make(map[string]Coordinate)
 
-// Boeing 777-300ER Engine
-// Max Level Speed (at altitude)575 mph (930 km/h) at 35,000 ft (10,675 m), Mach 0.87
+// Kps (kilometers per second) is the MAX LEVEL speed a Boeing 777-300ER Engine
+// can reach at a height of 35,000 ft
 const Kps = 0.2583333333 // kilometer per second assuming max speed of (930 km/h)
 
-//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//:::                                                                         :::
-//:::  This routine calculates the distance between two points (given the     :::
-//:::  latitude/longitude of those points). It is being used to calculate     :::
-//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
-//:::                                                                         :::
-//:::  Definitions:                                                           :::
-//:::    South latitudes are negative, east longitudes are positive           :::
-//:::                                                                         :::
-//:::  Passed to function:                                                    :::
-//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
-//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
-//:::    unit = the unit you desire for results                               :::
-//:::           where: 'M' is statute miles (default)                         :::
-//:::                  'K' is kilometers                                      :::
-//:::                  'N' is nautical miles                                  :::
-//:::                                                                         :::
-//:::  Worldwide cities and other features databases with latitude longitude  :::
-//:::  are available at https://www.geodatasource.com                         :::
-//:::                                                                         :::
-//:::  For enquiries, please contact sales@geodatasource.com                  :::
-//:::                                                                         :::
-//:::  Official Web site: https://www.geodatasource.com                       :::
-//:::                                                                         :::
-//:::               GeoDataSource.com (C) All Rights Reserved 2018            :::
-//:::                                                                         :::
-//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
+// GetRealDistance calculates the distance between two points (given the
+// latitude/longitude of those points). It is being used to calculate
+// the distance between two locations using GeoDataSource (TM) prodducts
+// Definitions:
+// South latitudes are negative, east longitudes are positive
+// Passed to function:
+// lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)
+// lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)
+// unit = the unit you desire for results
+// where: 'M' is statute miles (default)
+// 'K' is kilometers
+// 'N' is nautical miles
 func GetRealDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ...string) float64 {
 	const PI float64 = 3.141592653589793
 
@@ -66,7 +50,7 @@ func GetRealDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, uni
 	theta := float64(lng1 - lng2)
 	radtheta := float64(PI * theta / 180)
 
-	dist := math.Sin(radlat1) * math.Sin(radlat2) + math.Cos(radlat1) * math.Cos(radlat2) * math.Cos(radtheta)
+	dist := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
 
 	if dist > 1 {
 		dist = 1
@@ -86,45 +70,47 @@ func GetRealDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, uni
 	return dist
 }
 
-func GetTime(t1, t2 time.Time) (float64) {
-  diff := t2.Sub(t1)
-  return diff.Seconds()
+// GetTime calculates the difference of Final time and Initial time
+func GetTime(t1, t2 time.Time) float64 {
+	diff := t2.Sub(t1)
+	return diff.Seconds()
 }
 
 // Math for calculating if transaction is pbysically (commenrcially, not military) possible
 // Assuming the maximum distance somone can travel (in KM) k/s * s
-func getPotentialDistance(kps float64, deltaSeconds float64) float64{
-  return(kps*deltaSeconds)
+func getPotentialDistance(kps float64, deltaSeconds float64) float64 {
+	return (kps * deltaSeconds)
 }
 
 // real distance should always be = or <= to potential
 // distance (ideal distance that you can travel with ideal conditions)
 func isPosible(realDistance, potentialDistance float64) bool {
-  if (realDistance > potentialDistance) {
-    return false
-  }
-  return true
+	if realDistance > potentialDistance {
+		return false
+	}
+	return true
 }
 
 func init() {
-  citiesFile, err := os.OpenFile("worldcities.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
-  if err != nil {
-    panic(err)
-  }
-  defer citiesFile.Close()
+	citiesFile, err := os.OpenFile("worldcities.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer citiesFile.Close()
 
-  cities := []*City{}
+	cities := []*City{}
 
-  if err := gocsv.UnmarshalFile(citiesFile, &cities); err != nil { // Load cities from file
-    panic(err)
-  }
+	if err := gocsv.UnmarshalFile(citiesFile, &cities); err != nil { // Load cities from file
+		panic(err)
+	}
 
-  for _, city := range cities {
-    citiesDB[s.ToLower(city.Name+city.CountryCode)] = Coordinate{Lat: city.Lat, Lng: city.Lng }
-    // city.Lat: city.Lat, city.Lng: city.Lng}
-  }
+	for _, city := range cities {
+		citiesDB[s.ToLower(city.Name+city.CountryCode)] = Coordinate{Lat: city.Lat, Lng: city.Lng}
+	}
 }
 
+// GetCitiesDB returns a map with a key [CityName + CountryCode]
+// and a value of type Coordinate
 func GetCitiesDB() map[string]Coordinate {
-  return citiesDB
+	return citiesDB
 }
